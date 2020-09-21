353fba648b4ddec18407d991073768449f62f820
ehmicky <ehmicky@gmail.com>
2020-09-21 21:30:04 +0200

    Add support for `build.edge_handlers`

--- test/main.js
+++ test/main.js
@@ -5,6 +5,8 @@ const { callHandler } = require("./helpers/handler");
 const { runNetlifyBuild } = require("./helpers/run");
 
 const INTEGRATION_TEST_DIR = `${__dirname}/../integration-test`;
+const FIXTURES_DIR = `${__dirname}/fixtures`;
+const CONFIG_FIXTURE_DIR = `${FIXTURES_DIR}/config-dir`;
 
 test("Edge handlers should be bundled", async (t) => {
   await runNetlifyBuild(t, INTEGRATION_TEST_DIR);
@@ -19,4 +21,11 @@ test("Edge handlers should be bundled", async (t) => {
     getRequest: () => ({ headers: { get: () => "AK" } }),
   });
   t.deepEqual(helloWorld.logs, "1,2,3,4,379");
+});
+
+test("Edge handlers directory can be configured using build.edge_handlers", async (t) => {
+  await runNetlifyBuild(t, CONFIG_FIXTURE_DIR);
+
+  const { manifest } = loadBundle(t, CONFIG_FIXTURE_DIR);
+  t.deepEqual(manifest.handlers, ["example"]);
 });
