15cd859979f9613e9c375f3aeb091bd8413184f5
ehmicky <ehmicky@gmail.com>
2020-09-25 16:37:17 +0200

    Move directory

--- test/main.js
+++ test/main.js
@@ -4,8 +4,8 @@ const { loadBundle } = require("./helpers/bundle");
 const { callHandler } = require("./helpers/handler");
 const { runNetlifyBuild } = require("./helpers/run");
 
-const INTEGRATION_TEST_DIR = `${__dirname}/../integration-test`;
 const FIXTURES_DIR = `${__dirname}/fixtures`;
+const INTEGRATION_TEST_DIR = `${FIXTURES_DIR}/integration-test`;
 const CONFIG_FIXTURE_DIR = `${FIXTURES_DIR}/config-dir`;
 const WRONG_CONFIG_FIXTURE_DIR = `${FIXTURES_DIR}/wrong-config-dir`;
 
